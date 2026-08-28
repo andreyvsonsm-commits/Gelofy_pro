f9a83b582be5daa51fae9d0f4260455424ec3e0a
andreyvsonsm-commits <andreyvsonsm@gmail.com>
2026-08-28 15:38:14 -0300

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+// Cole aqui as duas informações que você anotou no Guia de Setup (Etapa 2):
+// Project Settings > API > Project URL / anon public key
+
+window.SUPABASE_URL = "https://suawlxdyryogtkbiwmma.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_Ib2TxbWSjj1oqZvXVbmVZQ_qE1tHol9";
